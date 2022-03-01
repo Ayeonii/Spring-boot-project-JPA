9ce2a65475c20aa70b2d16927d52d7af9875b737
Ayeonii <dldkekdusz@naver.com>
2022-03-01 16:39:47 +0900

    4.4. 로그인/로그아웃 구현하기

--- ch.2/shop/src/main/java/com/shop/config/SecurityConfig.java
+++ ch.2/shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 /*
 4.1 스프링 시큐리티 소개
 * 애플리케이션의 보안에서 중요한 두 가지 영역은 ‘인증’과 ‘인가’입니다.
@@ -20,13 +25,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends  WebSecurityConfigurerAdapter {
 
+    @Autowired
+    MemberService memberService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.formLogin()
+                .loginPage("/members/login")        // 로그인페이지 url tjfwjd
+                .defaultSuccessUrl("/")             // 로그인 성공 시 이동할 url
+                .usernameParameter("email")         // 로그인 시 사용할 파라미터 이름
+                .failureUrl("/members/login/error") // 로그인 실패 시 이동할 url
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) //로그아웃 url
+                .logoutSuccessUrl("/");             // 로그아웃 성공 시 이동할 url
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() { //비번을 DB에 그대로 저장하면, 해킹당할 경우 그대로 정보 노출. -> 암호화
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //Sring Security에서 인증은 AuthenticationManager를 통해 이루어짐.
+        //userDetailService를 구현하고 있는 객체로 memberService를 지정해주며, 비밀번호 암호화를 위해 passwordEncoder를 지정함.
+        auth.userDetailsService(memberService)
+                .passwordEncoder(passwordEncoder());
+
     }
 }
--- ch.2/shop/src/main/java/com/shop/service/MemberService.java
+++ ch.2/shop/src/main/java/com/shop/service/MemberService.java
@@ -6,11 +6,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+/*
+* <====UserDetailService====>
+* 데이터베이스에서 회원 정보를 가져오는 역할을 담당.
+* loadUserByUsername() 메소드가 존재하며, 회원 정보를 조회하여 사용자의 정보와 권한을 갖는 UserDetails 인터페이스를 반환.
+* 스프링 시큐리티에서 UserDetailService를 구현하고 있는 클래스를 통해 로그인 기능을 구현한다고 생각하면 된다.
+*
+* <====UserDetail====>
+* 스프링 시큐리티에서 회원의 정보를 담기 위해서 사용하는 인터페이스
+* 이 인터페이스를 직접 구현하거나 스프링 시큐리티에서 제공하는 User 클래스를 사용
+* User 클래스는 UserDetails 인터페이스를 구현하고 있는 클래스
+* */
+
 @Service
 @Transactional  //비즈니스 로직을 담당하는 서비스 계층 클래스에 ©Transactional 어노테이션을 선언합니다.
                 // 로직을 처리하다가 에러가 발생하였다면, 변경된 데이터를 로직을 수행하기 이전 상태로 콜백 시켜줍니다.
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     /*
     * 빈을 주입하는 방법으로는 @Autowired 어노테이션을 이용하거나, 필드 주입(Setter 주입), 생성자 주입을 이용 하는 방법이 있습니다.
@@ -29,6 +47,21 @@ public class MemberService {
         if (findMember != null) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()                           //UserDetail을 구현하고 있는 User 객체를 반환해준다.
+                .username(member.getName())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 
 }
