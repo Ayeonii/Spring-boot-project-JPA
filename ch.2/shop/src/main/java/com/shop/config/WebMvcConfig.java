cf967f05537d78789bc67cee29a95a32490196ef
Ayeonii <dldkekdusz@naver.com>
2022-03-26 21:58:26 +0900

    6.4 메인페이지

--- ch.2/shop/src/main/java/com/shop/config/WebMvcConfig.java
+++ ch.2/shop/src/main/java/com/shop/config/WebMvcConfig.java
@@ -13,6 +13,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**") // 웹브라우저에 입력하는 URL 에 /images 로 시작하는 경우 uploadPath 에 설정한 폴더를 기준으로 파일을 읽어오도록 설정
-                .addResourceLocations(uploadPath); // 로컬 컴퓨터에 저장된 파일을 읽어올 root 경로를 설정
+                .addResourceLocations(uploadPath);             // 로컬 컴퓨터에 저장된 파일을 읽어올 root 경로를 설정
     }
 }
